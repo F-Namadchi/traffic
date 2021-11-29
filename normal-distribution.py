58756ae551956d3217da875d97a636ba72ee2340
F-Namadchi <fnamadchi@yahoo.com>
2021-11-29 11:43:23 +0330

    add new space

--- normal-distribution.py
+++ normal-distribution.py
@@ -1,4 +1,6 @@
 from scipy.stats import truncnorm
+import scipy.stats as stats
+import numpy as np
 
 # define a function to create random variables with normal distribution
 # you should enter mean, standard deviation, lowerbound and upperbound as an inputs
@@ -8,5 +10,10 @@ def get_truncated_normal(mean=0, sd=1, low=0, upp=10):
         (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
 
 #------------------------------------------------------------------------------
-samples = get_truncated_normal(mean=333.529, sd=9.1402, low=328, upp=354)
-samplea.rvs(17)
+samples = get_truncated_normal(mean=98.9, sd=111.697, low=40, upp=322)
+samples = samples.rvs(5)
+
+N = stats.norm(loc=98.9, scale=111.697)
+N = N.random_list(5, [40, 322])
+
+#--------------
